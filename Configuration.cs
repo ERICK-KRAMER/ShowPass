d0944a14431c098e547c8807163827b64d096c89
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-09-06 00:37:40 -0300

    feat: add TokenService with JWT

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+namespace ShowPass
+{
+    public class Conficuration
+    {
+        public static string PrivateKey { get; set; } = "93587149-6e87-45f7-b1bd-6faae9a0e24c";
+    }
+}
---
+++ Services/TokenService.cs
@@ -0,0 +1,48 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using ShowPass.Models;
+
+namespace ShowPass.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            // cria uma intancia de JwtSecurityTokenHandler
+            var handler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(Conficuration.PrivateKey);
+
+            var credentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = credentials,
+                Subject = GenerateClaims(user),
+                Expires = DateTime.UtcNow.AddHours(2),
+            };
+
+            // gera token 
+            var token = handler.CreateToken(tokenDescriptor);
+
+            // Gera uma string do token 
+            var strToken = handler.WriteToken(token);
+
+            return strToken;
+        }
+
+        private static ClaimsIdentity GenerateClaims(User user)
+        {
+            var ci = new ClaimsIdentity();
+
+            ci.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+
+            return ci;
+
+        }
+    }
+}
